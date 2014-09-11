930d664aaff3b7d948b31a927218592ccac6ce6f
karloid <krldboyan@gmail.com>
2014-09-11 22:42:30 +0400

    Work in progress

--- src/com/example/ContactsTest/ContactListActivity.java
+++ src/com/example/ContactsTest/ContactListActivity.java
@@ -1,13 +1,16 @@
 package com.example.ContactsTest;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.Contacts;
 import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by Andrey on 9/11/2014.
@@ -17,6 +20,32 @@ public class ContactListActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact_list);
+        loadContacts();
+    }
+
+    private void loadContacts() {
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                if (Integer.parseInt(cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        Toast.makeText(this, "Name: " + name + ", Phone No: " + phoneNo, Toast.LENGTH_SHORT).show();
+                    }
+                    pCur.close();
+                }
+            }
+        }
     }
 
     @Override
@@ -32,8 +61,15 @@ public class ContactListActivity extends Activity {
             case R.id.add_new_contact:
                 addNewContact();
                 return true;
+            case R.id.sign_out:
+                returnToLogin();
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void returnToLogin() {
+        finish();
     }
 
     private void addNewContact() {
