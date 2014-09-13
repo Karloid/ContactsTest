17d674c9547915a0c34588963a876c7fbc730948
karloid <krldboyan@gmail.com>
2014-09-13 12:32:13 +0400

    login button states update

--- src/com/krld/ContactsTest/LoginActivity.java
+++ src/com/krld/ContactsTest/LoginActivity.java
@@ -4,21 +4,30 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import com.example.ContactsTest.R;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
     private View loginButton;
+    private EditText userNameEdit;
+    private EditText passwordEdit;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         hideActionBar();
-        initLoginButton();
+        initViews();
     }
 
-    private void initLoginButton() {
+    private void initViews() {
+        userNameEdit = (EditText) findViewById(R.id.username_edit);
+        userNameEdit.addTextChangedListener(new TextChangedListener());
+        passwordEdit = (EditText) findViewById(R.id.password_edit);
+        passwordEdit.addTextChangedListener(new TextChangedListener());
         loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,4 +50,25 @@ public class LoginActivity extends Activity {
         ActionBar actionBar = getActionBar();
         actionBar.hide();
     }
+
+    private void updateLoginButtonState() {
+        loginButton.setEnabled(!userNameEdit.getText().toString().isEmpty() && !passwordEdit.getText().toString().isEmpty());
+    }
+
+    private class TextChangedListener implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            updateLoginButtonState();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+    }
+
+
 }
